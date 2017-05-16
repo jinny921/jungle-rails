260aee9285741f0d930b705b13047f038f385e5d
Jinny Huang <jinny921@gmail.com>
2017-05-16 21:54:07 +0000

    tested add cart feature, number in cart

---
+++ spec/features/add_to_carts_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.feature "AddToCarts", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "Users can add product to cart" do
+    visit root_path
+    first('article.product footer').find_link('Add').click
+
+    save_screenshot
+
+    # within '#navbar' do
+    #   expect(page).to have_content 'My Cart (1)'
+    # end
+    
+    assert_selector('.cart-num', text: 'My Cart (5)')
+
+
+  end
+  
+
+end
